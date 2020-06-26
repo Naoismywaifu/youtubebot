f3cea28c8c123ccd695e2d062c9e4a2217265ac5
HiiZun <HiiZun@protonmail.com>
2020-06-26 13:17:33 +0200

    shard reconnecting err

--- events/shardReconnecting.js
+++ events/shardReconnecting.js
@@ -10,35 +10,5 @@ var embed = new Discord.MessageEmbed()
 .setColor("ORANGE")
 hook.send(embed)
 
-check247()
-
-async function check247() {
-
-    for (let i=0; i < client.radiomanager.all().length; i++){
-        const out = client.radiomanager.all()[i].ID
-        const guildid = out
-        const channelid = client.radiomanager.get(`${out}.voicechannel`)
-        const url = client.radiomanager.get(`${out}.url`)
-        const title = client.radiomanager.get(`${out}.title`)
-    
-        if(!client.guilds.cache.get(guildid)) return;
-        if(!client.channels.cache.get(channelid)) return client.radiomanager.delete(guildid);
-    
-        if(!client.guilds.cache.get(guildid).me.voice.channel){
-            let connection = await client.channels.cache.get(channelid).join()
-            connection.play(url)
-        }
-    
-        if(!client.guilds.cache.get(guildid).me.voice.connection){
-            let connection = await client.guilds.cache.get(guildid).me.voice.channel.join()
-            connection.play(url, {
-                volume: 1,
-              })
-        }
-    
-    }
-    
-    }
-
 
 }
