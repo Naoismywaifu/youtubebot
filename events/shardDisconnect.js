ffa631e8a58104f93d596206b78ae402954807c0
HiiZun <contact@hiizun.me>
2022-07-18 23:23:15 +0200

    disconnect shard removing process killing

--- events/shardDisconnect.js
+++ events/shardDisconnect.js
@@ -8,7 +8,7 @@ class ShardDisconnect {
 
         this.client.logger.shardEvent(this.client, shard+1, "disconnected", null)
         this.client.poster.stop()
-        return this.client.shard.broadcastEval(`if (this.shard.ids[0] === ${shard}) process.exit(0);`);
+        //return this.client.shard.broadcastEval(`if (this.shard.ids[0] === ${shard}) process.exit(0);`);
 
 
     }
